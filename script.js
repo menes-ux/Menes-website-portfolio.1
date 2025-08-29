e97e5f229035af6e9794ade9934004d77fb6830e
menes-ux <m.adisso@alustudent.com>
2025-08-29 03:59:21 +0200

    dark/light mode

--- script.js
+++ script.js
@@ -15,10 +15,33 @@
 
 
 // Toggle dark and light mode
-function toggleDarkLightMode() {
-    const body = document.body;
-    body.classList.toggle('dark-mode');
+// Get current mode from localStorage
+let darkmode = localStorage.getItem('darkmode');
+const themeSwitch = document.getElementById('themeswitch');
+
+// Enable dark mode
+const enableDarkmode = () => {
+    document.body.classList.add('darkmode');
+    localStorage.setItem('darkmode', 'active');
 }
+
+// Disable dark mode
+const disableDarkmode = () => {
+    document.body.classList.remove('darkmode');
+    localStorage.removeItem('darkmode');
+}
+
+// Apply dark mode if previously active
+if(darkmode === "active") enableDarkmode();
+
+// Toggle on click
+themeSwitch.addEventListener("click", () => {
+    darkmode = localStorage.getItem('darkmode');
+    darkmode !== "active" ? enableDarkmode() : disableDarkmode();
+});
+
+
+
 
 
 
